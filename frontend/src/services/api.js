00a1f2e3a6bf850dc0ca0f6665319038efc2a576
Imran <imranansari954830@gamil.com>
2026-01-16 04:52:22 +0530

    Connect frontend wtih live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,8 @@ import axios from "axios";
  Aage jaakar yahin se saare backend calls honge
 */
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL:"https://drivesafe-ai.onrender.com",
 });
 
 export default API;
